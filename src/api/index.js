af272f70ea0d6274515684b80d95e7ca4b15b041
Your Name <you@example.com>
2021-05-21 11:04:30 +0800

    修改

--- src/api/index.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-// 获取轮播图
-export function swiperList(data) {
-  return request({
-    url: '/api/client/home/carouselList',
-    method: 'post',
-    data: { type: data }
-  })
-}
